7420c0b4f6fb141b990d0a1c8d442f895f1d4a0d
duoli <zhaody901@126.com>
2021-11-23 19:43:17 +0800

    task # add logo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import { Link } from 'umi';
 import styles from './index.less';
+import imgLoading from '@/assets/images/logo.png'
 
 export default () => {
-  return (
-    <div>
-      <Link to={{ pathname: `/home` }}>
-        {' '}
-        <button className={styles.title}>Home</button>{' '}
-      </Link>
-      <Link to={{ pathname: `/user` }}>
-        <button className={styles.title}>User</button>{' '}
-      </Link>
-    </div>
-  );
+    return (
+        <div>
+            <Link to={{ pathname: `/home` }}>
+                {' '}
+                <button className={styles.title}>Home</button>
+                {' '}
+            </Link>
+            <Link to={{ pathname: `/user` }}>
+                <button className={styles.title}>User</button>
+                {' '}
+            </Link>
+            <div>
+                <img src={imgLoading} alt="OK"/>
+            </div>
+        </div>
+
+    );
 };
